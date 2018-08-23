1c5fe2b2a2788c5e0833f09ee95bbdd675cb21fe
fippli <filip.carl.johansson@gmail.com>
2018-08-22 20:25:51 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//
+// Get decimal places
+//
+const getDecimalPlaces = number => {
+    return number.toString().indexOf(".") === -1 ? 0 : number.toString().split(".")[1].length
+};
+
+//
+// Random integer
+//
+const randomInteger = ( min, max ) => {
+    return Math.floor((Math.random() * max) + min);
+};
+
+//
+// Create range between numbers
+// with min and max inclusive.
+//
+const createRange = ( min, max, step = 1 ) => {
+
+    if ( typeof min === "number" && typeof max === "number" && typeof step === "number" ) {
+        const interval = [ min ];
+        let i = min;
+        while ( min < max ? i + step <= max : i - step >= max  ) {
+            min < max ? i += step : i -= step;
+
+            interval.push( i.toFixed( getDecimalPlaces( step ) ) );
+        }
+        return interval;
+    } else {
+        throw new Error(`
+    The given parameters have got the wrong type: 
+    min: ${min} is ${typeof min} ${typeof min !== "number" ? "[NOT OK]" : "[OK]"} 
+    max: ${max} is ${typeof max} ${typeof max !== "number" ? "[NOT OK]" : "[OK]"}
+    step: ${max} is ${typeof step} ${typeof step !== "number" ? "[NOT OK]" : "[OK]"}`
+        );
+    }
+};
+
+module.exports = {
+    randomInteger: randomInteger,
+    createRange: createRange
+};
